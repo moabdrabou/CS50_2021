22f8982d398339d30ea09a2480bff9fcbaefe386
Mohammed Abdrabou <12809531+moabdrabou@users.noreply.github.com>
2021-03-17 15:02:23 +0300

    Create mario.py

---
+++ PSET6/mario-more/mario.py
@@ -0,0 +1,33 @@
+# prototype
+def main():
+
+    height = get_height()
+    draw(height, height)
+
+
+# printing the pyramid
+def draw(height, h):
+    if height == 0:
+        return
+    draw(height - 1, h)
+    print(" " * (h - height), end='')
+    print("#" * height, end='')
+    print("  ", end='')
+    print("#" * height)
+
+
+# prompting the user for height
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if (height > 0 and height < 9):
+                break
+        except ValueError:
+            None
+
+    return height
+
+
+# retun main function
+main()
