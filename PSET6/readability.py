73a8348a14aad01a6338553ed799154c384ac756
Mohammed Abdrabou <12809531+moabdrabou@users.noreply.github.com>
2021-03-18 12:52:27 +0300

    Create readability.py

---
+++ PSET6/readability.py
@@ -0,0 +1,44 @@
+# Prototype
+import string
+import re
+from cs50 import get_string
+
+
+# Prompting the user for text
+def main():
+
+    text = get_string("Text: ")
+
+    letters = len(re.findall('[a-zA-Z]', text))
+    words = len(text.split())
+    sentences = len(re.findall(r'[.!?]+', text))
+
+    index = readability_index(letters, words, sentences)
+
+    print_grade(index)
+
+
+# Calculation
+def readability_index(letters, words, sentences):
+
+    words_per_100 = words / 100
+    l = letters / words_per_100
+    s = sentences / words_per_100
+
+    index = round(0.0588 * l - 0.296 * s - 15.8)
+
+    return index
+
+
+# Printing the grade
+def print_grade(index):
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
+
+
+# Returning main function
+main()
