1e7dbeae124e3adb71b608e90e6d22c974ea19bf
Mohammed Abdrabou <12809531+moabdrabou@users.noreply.github.com>
2021-03-21 20:49:55 +0300

    Update 7.sql

--- PSET7/Movies/7.sql
+++ PSET7/Movies/7.sql
@@ -1,7 +1,5 @@
-SELECT name
-FROM people
-WHERE id IN (
-SELECT DISTINCT stars.person_id
-FROM stars JOIN movies ON movies.id = stars.movie_id
-WHERE year = "2004")
-ORDER BY birth
+
+SELECT title, rating FROM
+    movies JOIN ratings ON movies.id = ratings.movie_id
+WHERE year = "2010"
+ORDER BY rating DESC, title
